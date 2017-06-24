7ffb8aeffb9918ba9735e444d69636fa07ae26b9
Theophilus Omorebee <theo4u@ymail.com>
2017-06-24 21:56:51 +0100

    material ui installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import 'hammerjs';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
